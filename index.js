418524d6475b3738c8f880364ff78b35288d7cf8
Karthikeyan Y <karthikeyan.y@BLRETV-C02F2NK7.local>
2021-12-30 21:39:26 +0530

    revamp

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
 //typewriter text
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="text-wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
-  };
+    this.loopNum++;
+    delta = 500;
+  }
 
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
 
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
-
-    this.el.innerHTML = '<span class="text-wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-  };
+  }
+};
 
 
-  //download resume
+//download resume
 
-document.querySelector('.upload').addEventListener('click', function(ev){
-    ev.preventDefault();
-    var self = this;
-    self.classList.add('loading');
-    setTimeout(function() {
-        self.classList.remove('loading');
-        window.open('./files/Karthikeyan-SoftwareEngineer-resume.pdf')
-    }, 4200);
+document.querySelector('.upload').addEventListener('click', function (ev) {
+  ev.preventDefault();
+  var self = this;
+  self.classList.add('loading');
+  setTimeout(function () {
+    self.classList.remove('loading');
+    window.open('./files/Karthikeyan-SoftwareEngineer-resume.pdf')
+  }, 4200);
 })
 
 
 var header = document.querySelector(".header");
 var actualHeaderPos = header.offsetTop;
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 var prev = header.offsetTop;
 function myFunction() {
   if (window.scrollY >= actualHeaderPos) {
     header.classList.add("sticky");
-  } else if(window.scrollY <= actualHeaderPos){
+  } else if (window.scrollY <= actualHeaderPos) {
     header.classList.remove("sticky");
   }
 }
